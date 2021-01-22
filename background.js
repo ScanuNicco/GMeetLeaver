2e685efb62a7b2d9ce43a213abb3ef201ea12037
ScanuNicco <Scanuproductions@gmail.com>
2021-01-21 16:46:03 -0800

    Initial commit

---
+++ background.js
@@ -0,0 +1,27 @@
+//This script runs in the background, and closes tabs when their expiration time passes
+setInterval(checkTabs, 15000);
+
+function checkTabs() {
+	var tabtimes = []; // Stores tab IDs and the time to close them.
+
+	chrome.storage.local.get(['tabtimes'], function (result) {
+		tabtimes = result.tabtimes;
+		if (tabtimes == undefined) {
+			tabtimes = [];
+		}
+		var d = new Date();
+		var time = d.getHours() + ":" + d.getMinutes();
+		for (var i = 0; i < tabtimes.length; i++) {
+			if (tabtimes[i].expires == time) {
+				//This tab's time is up!
+				chrome.tabs.remove(Number(tabtimes[i].id), function () {
+					//RIP tab, remove it from the array to prevent errors
+					tabtimes.splice(i, 1);
+					chrome.storage.local.set({
+						'tabtimes': tabtimes
+					}, function () {});
+				});
+			}
+		}
+	});
+}
---
+++ popup.js
@@ -0,0 +1,89 @@
+var tabtimes = []; // Stores tab IDs and the time to close them.
+var nonmeets = false;
+
+document.getElementById("othertabsbox").addEventListener('click', otherTabs, false);
+
+chrome.storage.local.get(['tabtimes', 'othertabs'], function (result) {
+	tabtimes = result.tabtimes;
+	nonmeets = result.othertabs;
+	if(tabtimes == undefined){
+		tabtimes = [];
+	}
+	if(nonmeets == undefined){
+		nonmeets = false;
+	}
+	document.getElementById("othertabsbox").checked = nonmeets;
+	getMeetings(tabtimes);
+});
+
+function getMeetings(times) {
+	console.log(times);
+	chrome.tabs.query({
+		windowId: chrome.windows.WINDOW_ID_CURRENT
+	}, (tabs) => {
+		var numentries = 0;
+		for (var i = 0; i < tabs.length; i++) {
+			if (tabs[i].title.includes("Meet") || tabs[i].title.includes("BigBlueButton") || nonmeets) {
+				numentries++;
+				var time = -1;
+				for (var j = 0; j < times.length; j++) {
+					if (times[j].id == tabs[i].id) {
+						time = times[j].expires;
+					}
+				}
+				if (time == -1) { //No time has previously been set
+					document.getElementById("meetingsCont").innerHTML += "<div class='tab'><img src='" + tabs[i].favIconUrl + "' class='meeticon'><h2>" + tabs[i].title + "</h2><div class='actionsContainer'><input type='time' id='time" + i + "'><button class='actionButton' tabId='" + tabs[i].id + "' id='action" + i + "'>Set End Time</button></div></div>";
+					document.getElementById("action" + i).addEventListener('click', setTimer);
+				} else { //A time has beeen set for this tab, display it.
+					document.getElementById("meetingsCont").innerHTML += "<div class='tab'><img src='" + tabs[i].favIconUrl + "' class='meeticon'><h2>" + tabs[i].title + "</h2><div class='actionsContainer'><h3>Scheduled To Close At " + time + "</h3><button class='actionButton' tabId='" + tabs[i].id + "' id='close" + i + "'>Cancel</button></div></div>";
+					document.getElementById("close" + i).addEventListener('click', clearTimer);
+				}
+			}
+		}
+		if(numentries == 0){
+			document.getElementById("meetingsCont").innerHTML += "<h2 style='color: grey; margin: 15px;'>No Meetings Open</h2>";
+		}
+	});
+}
+
+function setTimer(evt) {
+	console.log('test');
+	tabID = evt.target.getAttribute("tabID");
+	time = document.getElementById("time" + evt.target.id.replace(/^\D+/g, '')).value;
+	if(time == ""){
+		alert("No Time Set!");
+		return;
+	}
+	tabtimes.push({
+		id: tabID,
+		expires: time
+	});
+	saveTimes();
+}
+
+function clearTimer(evt) {
+	tabID = evt.target.getAttribute("tabID");
+	for(var i = 0; i < tabtimes.length; i++){
+		if(tabtimes[i].id == tabID){
+			tabtimes.splice(i, 1);
+		}
+	}
+	saveTimes();
+}
+
+function saveTimes() { // Stores the tabtimes array using the chrome.storage API
+	chrome.storage.local.set({
+		'tabtimes': tabtimes
+	}, function () {
+		window.location.reload();
+	});
+}
+
+function otherTabs(){
+	nonmeets = !nonmeets;
+	chrome.storage.local.set({
+		'othertabs': nonmeets
+	}, function () {
+		window.location.reload();
+	});
+}
